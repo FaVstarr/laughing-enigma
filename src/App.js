b120247a32a1176db5214a2457fb77bdc9cb8b3c
Favboi <favourchamberlain32@gmail.com>
2023-01-29 21:16:56 +0100

    Blog

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message'
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './EventBind';
-import ParentComponent from './ParentComponent';
+// import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 function App() {
+
+  const title = 'Welcome to the new blog';
+  const likes = 50;
   return (
     <div className="App">
-      <ParentComponent />
-      {/* <EventBind /> */}
-      {/* <Counter /> */}
-      {/* <Message  /> */}
-      {/* <Greet  name="Bruce" heroName="Batman">
-        <p>This is children props</p>
-      </Greet>
-      <Greet  name="Clark" heroName="Superman">
-        <button> Action</button>
-      </Greet> */}
-      {/* <FunctionClick /> */}
-      {/* <ClassClick /> */}
-      {/* <Greet  name="Diana" heroName="Wonder Woman"/>
-     <Welcome name="Bruce" heroName="Batman"/>  */}
-      {/* <Welcome name="Clark" heroName="Superman"/> 
-      <Welcome name="Diana" heroName="Wonder Woman"/> 
-      <Hello />  */}
+      <Navbar />
+     <div className="content">
+      <Home />
+     </div>
     </div>
   );
 }
