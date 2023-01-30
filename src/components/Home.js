dc07bb9d7fde582804ccbcc90c5416c6a3291528
Favboi <favourchamberlain32@gmail.com>
2023-01-30 08:59:16 +0100

    Jan 30th

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -8,10 +9,21 @@ const Home = () => {
 
     ]);
 
-    
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+    }
+
+    useEffect(() => {
+        console.log('use effect ran')
+        console.log(blogs)
+    })
+
     return ( 
         <div className="home">
-            
+           <BlogList blogs={blogs} title = "All Blogs" handleDelete={handleDelete} />
+           <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title = "Mario's Blogs" />
         </div>
      );
 }
